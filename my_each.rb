1acb7c68432b3b4b6c9274d124678d7cbb9ff70b
Kimberly Baker <bakerk2010@gmail.com>
2020-09-20 22:46:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,19 +1,8 @@
-def my_each(array) # put argument(s) here
-  # code here
-end
-
-def hello_t(array)
-  collection = [1, 2, 3, 4]
-  my_each(collection) do |i|
-    puts i
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
-    while i < array.length
-      yield array[i]
-      i = i + 1
-    end
-  
-    array
-  else
-    puts "Hey!No block was given!"
-  end
+  array
 end
